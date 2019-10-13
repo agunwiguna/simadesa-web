1bf38a01d966ccf44522e31e17c252ebbab2dcf9
agunwiguna <wgnagun@gmail.com>
2019-10-13 20:59:03 +0700

    ubah xx

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,8 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$route['default_controller'] = 'AuthController/index';
-$route['404_override'] = '';
+$route['default_controller'] = 'GeneralController/index';
 $route['translate_uri_dashes'] = FALSE;
 
 //Auth
